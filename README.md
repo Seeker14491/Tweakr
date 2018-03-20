29831301817dbe404123229d04a7ff7653bf358a
Brian Bowman <seeker14491@gmail.com>
2018-03-20 18:25:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Tweakr
+
+A [Spectrum](https://github.com/Ciastex/Spectrum) plugin that provides useful tweaks and cheats.
+
+Cheats are disabled in multiplayer, and stop you from getting a time on the leaderboards.
+
+### Installation
+
+This plugin requires Harmony; download the release [from here](https://github.com/pardeike/Harmony/releases) and put `0Harmony.dll` in `<game_dir>\Distance_Data\Managed`. Then download this plugin from [the releases](https://github.com/Seeker14491/Tweakr/releases) page, and put it in `<game_dir>\Distance_Data\Spectrum\Plugins`. Start the game to generate the plugin configuration file at `<game_dir>\Distance_Data\Spectrum\Settings\Tweakr.json`, and then close the game and edit it. Most things are disabled by default, and there are no hotkeys set up.
+
+| Setting                        | Description                                                  |
+| ------------------------------ | ------------------------------------------------------------ |
+| enableCheatMenu                | Enables a built-in cheat menu that's normally hidden. It's accessible from the options menu. **Warning:** The Monster Truck cheat is buggy and leads to crashes. To prevent this, only turn it on while playing a track, and turn it back off before leaving the level. |
+| carScreenDeclutter             | Hides the large arrow that appears on the back of the car when you go off-track, as well as the placement widget that appears in multiplayer sprint that shows what position you are in the race. The compass is also visible during the initial countdown. |
+| disableWingGripControlModifier | Stops the grip button from affecting wing controls.          |
+| checkpointHotkey               | **(cheat)** A hotkey to set the car's respawn position. It currently gets overwritten if you hit a course checkpoint. |
+| infiniteCooldownHotkey         | **(cheat)** A hotkey to give infinite cooldown. Lasts until death. |
+| disableJetRampdownHotkey       | **(cheat)** A hotkey that stops the car jets from losing power. Lasts until death. |
+
+### Defining Hotkeys
+
+Hotkeys are defined in terms of ingame controls.
+
+#### Examples of valid hotkey definitions:
+
+`""`: hotkey disabled
+
+`"Horn"`: active when Horn is input
+
+`"Special+Horn"`: active when both Special and Horn inputs are active
+
+You can chain as many inputs as you want using `+`.
+
+
+
+#### Valid Inputs
+
+```
+Gas
+Brake
+SteerLeft
+SteerRight
+Boost
+Jump
+Wings
+Special
+WingRollLeft
+WingRollRight
+WingPitchDown
+WingPitchUp
+WingYawLeft
+WingYawRight
+JetRollLeft
+JetRollRight
+JetPitchDown
+JetPitchUp
+Grip
+AirRollLeft
+AirRollRight
+AirPitchDown
+AirPitchUp
+Pause
+Reset
+ShowScore
+Horn
+ChangeCameraView
+CenterCamera
+CameraYawLeft
+CameraYawRight
+CameraPitchDown
+CameraPitchUp
+SpectateNextPlayer
+MenuLeft
+MenuRight
+MenuDown
+MenuUp
+MenuPageLeft
+MenuPageRight
+MenuPageDown
+MenuPageUp
+MenuConfirm
+MenuCancel
+MenuDelete
+MenuStart
+Chat
+MenuCreatePlaylist
+MenuRateLevel
+MenuSorting
+CameraLookBehind
+ToggleMenuVisibility
+```
---
+++ Tweakr/Entry.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Spectrum.API;
+using Spectrum.API.Configuration;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+using Harmony;
+
+namespace Tweakr
+{
+    public class Entry : IPlugin, IUpdatable
+    {
+        public string FriendlyName => "Tweakr";
+        public string Author => "Seekr";
+        public string Contact => "Discord: Seekr#3274; Steam: Seeker14491";
+        public APILevel CompatibleAPILevel => APILevel.XRay;
+
+        private static Settings _settings;
+        private static readonly Dictionary<string, InputAction[]> Hotkeys = new Dictionary<string, InputAction[]>();
+
+        internal static bool Cheated;
+        internal static bool HasSetCheckpoint;
+        internal static bool JetRampdownDisabled;
+
+        private static readonly bool AllowGameplayCheatsInMultiplayer = false;
+
+        public void Initialize(IManager manager)
+        {
+            _settings = InitializeSettings();
+
+            var harmony = HarmonyInstance.Create("com.seekr.tweakr");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            if (_settings.GetItem<bool>("disableWingGripControlModifier"))
+            {
+                var original = typeof(WingsGadget).GetMethod("GadgetUpdateLocal", BindingFlags.Public | BindingFlags.Instance);
+                var prefix = typeof(DisableWingGripControlModifier).GetMethod("Prefix", BindingFlags.NonPublic | BindingFlags.Static);
+                harmony.Patch(original, new HarmonyMethod(prefix), null);
+            }
+
+            if (_settings.GetItem<bool>("enableCheatMenu"))
+            {
+                var original = typeof(CheatsManager).GetProperty("EnabledInThisBuild_", BindingFlags.Public | BindingFlags.Static).GetGetMethod();
+                var postfix = typeof(EnableCheatMenu).GetMethod("Postfix", BindingFlags.NonPublic | BindingFlags.Static);
+                harmony.Patch(original, null, new HarmonyMethod(postfix));
+            }
+        }
+
+        public void Update()
+        {
+            if (_settings.GetItem<bool>("carScreenDeclutter"))
+            {
+                var carScreenLogic = G.Sys.PlayerManager_?.Current_?.playerData_?.CarScreenLogic_;
+                if (carScreenLogic?.CarLogic_.IsLocalCar_ ?? false)
+                {
+                    carScreenLogic.arrow_ = carScreenLogic.compass_;
+                    if (Traverse.Create(carScreenLogic.placementText_).Field("renderer_").GetValue() != null)
+                    {
+                        carScreenLogic.placementText_.IsVisible_ = false;
+                    }
+                    carScreenLogic.ModeWidgetVisible_ = true;
+                }
+            }
+
+            if (!MultiplayerCheatShouldContinue())
+            {
+                return;
+            }
+
+            {
+                var transform = G.Sys.PlayerManager_?.Current_?.playerData_?.CarLogic_?.transform;
+                if (IsTriggered(_settings.GetItem<string>("checkpointHotkey")) && transform != null)
+                {
+                    G.Sys.PlayerManager_?.Current_?.playerData_?.SetResetTransform(transform.position, transform.rotation);
+                    HasSetCheckpoint = true;
+                }
+            }
+
+            if (IsTriggered(_settings.GetItem<string>("infiniteCooldownHotkey")))
+            {
+                G.Sys.PlayerManager_?.Current_?.playerData_?.CarLogic_?.SetInfiniteCooldown(true);
+                Cheated = true;
+            }
+
+            if (IsTriggered(_settings.GetItem<string>("disableJetRampdownHotkey")))
+            {
+                JetRampdownDisabled = true;
+            }
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        private static Settings InitializeSettings()
+        {
+            var settings = new Settings(typeof(Entry));
+
+            var entries = new[]
+            {
+                new SettingsEntry("enableCheatMenu", true),
+                new SettingsEntry("carScreenDeclutter", false),
+                new SettingsEntry("disableWingGripControlModifier", false),
+                new SettingsEntry("checkpointHotkey", ""),
+                new SettingsEntry("infiniteCooldownHotkey", ""),
+                new SettingsEntry("disableJetRampdownHotkey", ""),
+            };
+
+            foreach (var s in entries)
+            {
+                if (!settings.ContainsKey(s.Name))
+                {
+                    settings.Add(s.Name, s.DefaultVal);
+                }
+            }
+
+            settings.Save();
+            return settings;
+        }
+        
+        private static InputAction[] ParseHotkey(string keys)
+        {
+            if (keys == "")
+            {
+                return new InputAction[] {};
+            }
+
+            return keys
+                .Split('+')
+                .Select(x => (InputAction)Enum.Parse(typeof(InputAction), x, true))
+                .ToArray();
+        }
+
+        private static bool IsTriggered(string hotkey)
+        {
+            if (!Hotkeys.TryGetValue(hotkey, out var v))
+            {
+                v = ParseHotkey(hotkey);
+                Hotkeys.Add(hotkey, v);
+            }
+
+            return IsTriggered(v);
+        }
+
+        private static bool IsTriggered(ICollection<InputAction> hotkey)
+        {
+            if (hotkey.Count == 0)
+            {
+                return false;
+            }
+
+            return hotkey
+                .Select(x =>
+                    G.Sys.PlayerManager_?.Current_?.playerData_?.InputStates_.GetPressed(x) ??
+                    false)
+                .All(x => x);
+        }
+
+        internal static bool MultiplayerCheatShouldContinue()
+        {
+            return AllowGameplayCheatsInMultiplayer || !(G.Sys.NetworkingManager_?.IsOnline_ ?? false);
+        }
+    }
+
+    internal class SettingsEntry
+    {
+        public readonly string Name;
+        public readonly object DefaultVal;
+
+        public SettingsEntry(string name, object defaultVal)
+        {
+            Name = name;
+            DefaultVal = defaultVal;
+        }
+    }
+
+    [HarmonyPatch(typeof(CheatsManager))]
+    [HarmonyPatch("GameplayCheatsUsedThisLevel_", PropertyMethod.Getter)]
+    internal class BlockLeaderboardUpdatingWhenCheating
+    {
+        static void Postfix(ref bool __result)
+        {
+            __result |= Entry.Cheated;
+        }
+    }
+
+    // For some reason using the built-in cheats in multiplayer will not block leaderboard updating; this fixes that.
+    [HarmonyPatch(typeof(CheatsManager))]
+    [HarmonyPatch("AnyGameplayCheatEnabled_", PropertyMethod.Getter)]
+    internal class FixMultiplayerBuiltinCheats
+    {
+        static bool Prefix(CheatsManager __instance, ref bool __result)
+        {
+            __result = Traverse.Create(__instance).Field("anyGameplayCheatsUsedThisLevel_").GetValue<bool>();
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CarCheatBase), "AddCheatsToCarBlueprint")]
+    internal class RegulateMultiplayerBuiltinCheats
+    {
+        static bool Prefix(ref uint cheatFlags)
+        {
+            if (!Entry.MultiplayerCheatShouldContinue())
+            {
+                cheatFlags = 0;
+                Traverse.Create(G.Sys.CheatsManager_).Field("anyGameplayCheatsUsedThisLevel_").SetValue(false);
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(CarLogic), "OnEventCarDeath")]
+    internal class PatchCarDeath
+    {
+        static void Postfix()
+        {
+            if (Entry.HasSetCheckpoint)
+            {
+                Entry.Cheated = true;
+            }
+
+            Entry.JetRampdownDisabled = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(GameManager), "SceneLoaded")]
+    internal class PatchSceneLoaded
+    {
+        static void Postfix()
+        {
+            Entry.Cheated = false;
+            Entry.HasSetCheckpoint = false;
+            Entry.JetRampdownDisabled = false;
+        }
+    }
+
+    internal class DisableWingGripControlModifier
+    {
+        static bool Prefix(InputStates inputStates)
+        {
+            inputStates.Clear(InputAction.Grip);
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(JetsGadget), "GadgetFixedUpdate")]
+    internal class DisableJetsRampdown
+    {
+        static bool Prefix(JetsGadget __instance)
+        {
+            if (Entry.JetRampdownDisabled && Entry.MultiplayerCheatShouldContinue())
+            {
+                Traverse.Create(__instance).Field("thrusterBoostTimer_").SetValue(0f);
+                Entry.Cheated = true;
+            }
+            return true;
+        }
+    }
+
+    internal class EnableCheatMenu
+    {
+        static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
